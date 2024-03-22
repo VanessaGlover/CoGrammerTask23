dbecf4262d481b2f62830ad271b42e1048d5c1c1
VanessaGlover <vanessaglover.95@gmail.com>
2024-03-22 14:21:23 +0000

    task 2

---
+++ Task 2/number.py
@@ -0,0 +1,13 @@
+num1 = int (input ("Enter your first integer "))
+num2 = int (input ("Enter your second integer "))
+num3 = int (input ("Enter your third integer "))
+
+sum_of_numbers = num1 + num2 + num3
+minus_first_two_numbers = num1 - num2
+third_multiplied_by_first = num3 * num1
+sum_divided_by_third = sum_of_numbers / num3
+
+print(sum_of_numbers)
+print(minus_first_two_numbers)
+print(third_multiplied_by_first)
+print(sum_divided_by_third)
